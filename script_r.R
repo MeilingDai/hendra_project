780a9f281000b8f826339395a15632e023025e23
Dai <dai035@csiro.au>
2019-10-10 12:36:22 +1100

    commit

--- script_r.R
+++ script_r.R
@@ -15,5 +15,19 @@ field_horses_raw_counts <- read_csv("data/field_horses_raw_counts.csv")
 field_horses_raw_counts
 
 
+tidy_field_horses_raw_counts <-  field_horses_raw_counts %>%  
+  gather(key = horse_id, value = counts, -gene) %>% 
+  spread(key = gene, value = counts ) %>% 
+  gather(key = gene, value = counts, - horse_id)
+
+tidy_field_horses_raw_counts 
 
 
+
+full_data <- full_join(tidy_field_horses_raw_counts, field_horses_metadata)
+
+full_data
+
+ggplot (data = full_data,
+        aes(x = gene,y = counts, color = horse_id, shape = infection),
+        geom_point())
