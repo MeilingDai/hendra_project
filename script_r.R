2ecb5231043ae2bcda92736507c353d9809c6224
MeilingDai <55425606+MeilingDai@users.noreply.github.com>
2019-10-10 12:12:46 +1100

    Add files via upload

---
+++ script_r.R
@@ -0,0 +1,19 @@
+.libPaths(c("C:/Users/DAI035/data_school/Packages"))
+
+library(tidyverse)
+sessionInfo()
+
+
+read_csv("data/field_horses_metadata.csv")
+field_horses_metadata <- read_csv("data/field_horses_metadata.csv")
+field_horses_metadata
+
+
+#read csv data
+read_csv("data/field_horses_raw_counts.csv")
+field_horses_raw_counts <- read_csv("data/field_horses_raw_counts.csv")
+field_horses_raw_counts
+
+
+
+
