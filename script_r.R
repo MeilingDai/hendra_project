8582b4d4251b1558b50c6a89f7cfce72a7a0b6fe
Dai <dai035@csiro.au>
2019-10-10 15:13:43 +1100

    commit to save data and figures

--- script_r.R
+++ script_r.R
@@ -4,30 +4,57 @@ library(tidyverse)
 sessionInfo()
 
 
-read_csv("data/field_horses_metadata.csv")
-field_horses_metadata <- read_csv("data/field_horses_metadata.csv")
-field_horses_metadata
 
+read_csv("data/field_horses_metadata_modified.csv")
+field_horses_metadata_modified <- read_csv("data/field_horses_metadata_modified.csv")
+field_horses_metadata_modified 
 
 #read csv data
-read_csv("data/field_horses_raw_counts.csv")
-field_horses_raw_counts <- read_csv("data/field_horses_raw_counts.csv")
-field_horses_raw_counts
+read_csv("data/field_horses_raw_counts_modified.csv")
+field_horses_raw_counts_modified <- read_csv("data/field_horses_raw_counts_modified.csv")
+field_horses_raw_counts_modified 
 
 
-tidy_field_horses_raw_counts <-  field_horses_raw_counts %>%  
-  gather(key = horse_id, value = counts, -gene) %>% 
-  spread(key = gene, value = counts ) %>% 
-  gather(key = gene, value = counts, - horse_id)
+tidy_field_horses_raw_counts <-  field_horses_raw_counts_modified %>%  
+  gather(key = horse_id, value = counts, -gene) 
+tidy_field_horses_raw_counts
 
-tidy_field_horses_raw_counts 
+full_data <- full_join(tidy_field_horses_raw_counts, field_horses_metadata_modified)
+
+full_data 
+
+
+numeric_full_data <-  full_data %>% 
+  separate(col = gene, into = c("gene_cluster1", "gene_cluster2"), sep = "_" ) %>% 
+  mutate (gene_cluster1 = as.numeric(gene_cluster1), gene_cluster2 = as.numeric(gene_cluster2)) %>% 
+  filter(gene_cluster2 != "NA")
+  
+
+
+numeric_full_data
 
 
 
-full_data <- full_join(tidy_field_horses_raw_counts, field_horses_metadata)
+numeric_full_data_plot <- ggplot (data = numeric_full_data,
+        aes(x = gene_cluster2,
+            y = counts, 
+            color = horse_id,
+            group = gene_cluster2))+
+        geom_point()+
+  scale_y_continuous(limits = c(0, 1000))+
+  scale_color_manual(values = c("grey", "black", "darkgrey", "red","blue","green"))+
+  facet_wrap(~ horse_id) 
+ggsave(filename = "results/Figure 3.png", plot = numeric_full_data_plot, width = 10, height = 8, dpi = 300, units = "cm")
 
-full_data
+  
+numeric_full_data_plot <- ggplot (data = numeric_full_data,
+                                  aes(x = gene_cluster2,
+                                      y = counts, 
+                                      color = horse_id,
+                                      group = gene_cluster1))+
+  geom_point()+
+  scale_y_continuous()+
+  scale_color_manual(values = c("grey", "black", "darkgrey", "red","blue","green"))+
+  facet_wrap(~ horse_id) 
 
-ggplot (data = full_data,
-        aes(x = gene,y = counts, color = horse_id, shape = infection),
-        geom_point())
+ggsave(filename = "results/Figure 4.png", plot = numeric_full_data_plot, width = 10, height = 8, dpi = 300, units = "cm")
